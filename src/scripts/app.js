a034df11dc03ab2cc2c31cdc74dc5832a05a939e
rykitik <sasha2020419@gmail.com>
2023-09-20 21:55:10 +0300

    Закончил верхнее меню, модульное окно поиска

---
+++ src/scripts/app.js
@@ -0,0 +1,16 @@
+jQuery(function($){
+	$('.header-search-button, .header-search-button__icon').on('click', function(e){
+	  $('.header-search-window').addClass('header-search-window--show');
+	});
+	
+	$('.header-search-window').on('click', function(e){
+	  e.stopPropagation();
+	});
+	
+	$('body').on('click', function(e){
+	  if(e.target !== $('.header-search-window')[0] && e.target !== $('.header-search-button')[0] && e.target !== $('.header-search-button__icon')[0] && $('.header-search-window').hasClass('header-search-window--show') ){
+	  console.log(e.target);
+	  $('.header-search-window').removeClass('header-search-window--show');
+	  }
+	});
+});
