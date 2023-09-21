44933c9ef1366d1d5b67fd55766aacf316bd9411
rykitik <sasha2020419@gmail.com>
2023-09-21 18:20:57 +0300

    Затемление экрана

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,7 @@
 jQuery(function($){
 	$('.header-search-button, .header-search-button__icon').on('click', function(e){
 	  $('.header-search-window').addClass('header-search-window--show');
+	  $('.header-overlay').addClass('header-overlay--visible');
 	});
 	
 	$('.header-search-window').on('click', function(e){
@@ -9,8 +10,8 @@ jQuery(function($){
 	
 	$('body').on('click', function(e){
 	  if(e.target !== $('.header-search-window')[0] && e.target !== $('.header-search-button')[0] && e.target !== $('.header-search-button__icon')[0] && $('.header-search-window').hasClass('header-search-window--show') ){
-	  console.log(e.target);
-	  $('.header-search-window').removeClass('header-search-window--show');
+	  	$('.header-search-window').removeClass('header-search-window--show');
+		$('.header-overlay').removeClass('header-overlay--visible');
 	  }
 	});
 });
