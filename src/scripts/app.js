13b77f2b2ba5c799c13771c9cdc97af2652c6879
rykitik <sasha2020419@gmail.com>
2023-09-25 19:43:38 +0300

    Починил кнопки на других слайдах

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -15,54 +15,53 @@ jQuery(function($){
 	  }
 	});
 	$(document).ready(function() {
-		$('.main-intro-slider__link, .main-intro-slider__price').on('click', function(e){
-			$('.main-intro-window').addClass('main-intro-window--show');
-			$('.header-overlay').addClass('header-overlay--visible');
-		});
-		
-		$('.main-intro-window').on('click', function(e){
-			e.stopPropagation();
-		});
-		
-		$('body').on('click', function(e){
-			if(e.target !== $('.main-intro-window')[0] && e.target !== $('.main-intro-slider__link')[0] && $('.main-intro-window').hasClass('main-intro-window--show') ){
-				$('.main-intro-window').removeClass('main-intro-window--show');
-			$('.header-overlay').removeClass('header-overlay--visible');
-			}
-		});
-
 		// СЛАЙДЕР
-        let slides = $('.swiper-wrapper .swiper-slide');
+		let slides = $('.swiper-wrapper .swiper-slide');
 		let topLines = $('.main-intro-slider__nav .main-top__line');
-        let currentSlide = 0;
-
-        function showSlide(index) {
-            slides.hide();
-            slides.eq(index).show();
-        }
-
-        showSlide(currentSlide);
-
-        // Обработчики событий для кнопок
-
+		let currentSlide = 0;
+	  
+		function showSlide(index) {
+		  slides.hide();
+		  slides.eq(index).show();
+		}
+	  
+		showSlide(currentSlide);
+	  
+		// Обработчики событий для кнопок
 		$('.main-into-slider__left-btn').on('click', function(e){
-			topLines.eq(currentSlide).removeClass('main-top__line--active');
-			currentSlide--;
-            if (currentSlide < 0) {
-                currentSlide = slides.length - 1;
-            }
-            showSlide(currentSlide);
-			topLines.eq(currentSlide).addClass('main-top__line--active');
+		  topLines.eq(currentSlide).removeClass('main-top__line--active');
+		  currentSlide--;
+		  if (currentSlide < 0) {
+			currentSlide = slides.length - 1;
+		  }
+		  showSlide(currentSlide);
+		  topLines.eq(currentSlide).addClass('main-top__line--active');
 		});
-	
+	  
 		$('.main-into-slider__right-btn').on('click', function(e){
-			topLines.eq(currentSlide).removeClass('main-top__line--active');
-			currentSlide++;
-            if (currentSlide >= slides.length) {
-                currentSlide = 0;
-            }
-            showSlide(currentSlide);
-			topLines.eq(currentSlide).addClass('main-top__line--active');
+		  topLines.eq(currentSlide).removeClass('main-top__line--active');
+		  currentSlide++;
+		  if (currentSlide >= slides.length) {
+			currentSlide = 0;
+		  }
+		  showSlide(currentSlide);
+		  topLines.eq(currentSlide).addClass('main-top__line--active');
 		});
-	});
+	  
+		$(document).on('click', '.main-intro-slider__link, .main-intro-slider__price', function(e) {
+		  $('.main-intro-window').addClass('main-intro-window--show');
+		  $('.header-overlay').addClass('header-overlay--visible');
+		});
+	  
+		$('.main-intro-window').on('click', function(e) {
+		  e.stopPropagation();
+		});
+	  
+		$('body').on('click', function(e) {
+		  if (e.target !== $('.main-intro-window')[0] && !$(e.target).hasClass('main-intro-slider__link')) {
+			$('.main-intro-window').removeClass('main-intro-window--show');
+			$('.header-overlay').removeClass('header-overlay--visible');
+		  }
+		});
+	  });
 });
